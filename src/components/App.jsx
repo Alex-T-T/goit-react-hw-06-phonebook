8b9d884d29ef52bca099ac70b383f192e546f415
Olexandr Tolochko <0504fusion@gmail.com>
2022-10-23 09:41:31 +0300

    add auditName auditNumber

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,35 +4,28 @@ import { Filter } from './Filter/Filter';
 import { useSelector, useDispatch } from 'react-redux';
 import { addContact } from 'redux/contactsSlice';
 import { getContacts } from 'redux/selectors';
+import { auditName } from 'utils/auditName';
+import { auditNumber } from 'utils/auditNumber';
 
 export const App = () => {
     const dispatch = useDispatch();
     const contacts = useSelector(getContacts)
 
-
     const addNewContact = (data) => {
-        // console.log(data)
         const { name, number } = data;
-        const normalizeNewContactName = name.toLowerCase();
-        const findName = contacts?.find(contact => contact.name.toLowerCase() === normalizeNewContactName);
-        const findNumber = contacts?.find(contact => contact.number === number)
-            if (findName) {
-                return alert(`${name} is already in contacts.`);
-            }
-            if (findNumber) {
-                return alert(`${number} is already in contacts.`);
-            }
-            
+
+        if (auditName(contacts, name)) {
+            alert(`${name} is already in contacts.`);
+            return 
+        };
+
+        if (auditNumber(contacts, number)) {
+            alert(`${number} is already in contacts.`);
+            return 
+        };
+
         dispatch(addContact(data));
     }
-
-    // const getVisibleContacts = () => {
-    //     console.log('Filter numbers')
-        // const normalizeContactName = filter.toLowerCase();
-
-        // return contacts.filter(contact => contact.name.toLowerCase().includes(normalizeContactName))
-    // }
-
 
         return(
             <>
